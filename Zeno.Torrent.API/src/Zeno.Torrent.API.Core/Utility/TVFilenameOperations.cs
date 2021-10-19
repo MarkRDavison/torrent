e0988a4b9af066e831aacdaade49c98cc9eb7bc7
Mark Davison <markdavison0+github@gmail.com>
2021-10-20 12:06:48 +1300

    Allow episode numbers of 0

--- Zeno.Torrent.API/src/Zeno.Torrent.API.Core/Utility/TVFilenameOperations.cs
+++ Zeno.Torrent.API/src/Zeno.Torrent.API.Core/Utility/TVFilenameOperations.cs
@@ -9,7 +9,7 @@ namespace Zeno.Torrent.API.Core.Utility {
     public struct TVFilenameInfo {
         public bool Valid =>
             Season > 0 &&
-            Episode > 0;
+            Episode >= 0;
         public int Season { get; set; }
         public int Episode { get; set; }
         public string Quality { get; set; }
